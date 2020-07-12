7e3c18527a4e85728aa89d377dac768ca80a28fd
Friends233 <friends233@foxmail.com>
2020-07-12 20:47:51 +0800

    2020/7/12

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -36,6 +36,39 @@
             <p class="text">{{food.info}}</p>
           </div>
           <split></split>
+          <div class="rating">
+            <h1 class="title">商品评价</h1>
+            <rating-select
+              :ratings="ratings"
+              :onlyContent="onlyContent"
+              :select-type="selectType"
+              :desc="desc"
+              @select="onSelect"
+              @toggle="onToggle"
+            >
+            </rating-select>
+            <div class="rating-wrapper">
+              <ul v-show="computedRatings && computedRatings.length">
+                <li
+                  v-for="(rating,index) in computedRatings"
+                  class="rating-item border-bottom-1px"
+                  :key="index"
+                >
+                  <div class="user">
+                    <span class="name">{{rating.username}}</span>
+                    <img class="avatar" width="12" height="12" :src="rating.avatar">
+                  </div>
+                  <div class="time">{{format(rating.rateTime)}}</div>
+                  <p class="text">
+                    <span :class="{'icon-thumb_up':rating
+                    .rateType===0,'icon-thumb_down':rating
+                    .rateType===1}"></span>{{rating.text}}
+                  </p>
+                </li>
+              </ul>
+              <div class="no-rating" v-show="!computedRatings || !computedRatings.length">暂无评价</div>
+            </div>
+          </div>
         </div>
       </cube-scroll>
     </div>
@@ -46,16 +79,47 @@
   import Split from '../split/split'
   import popupMixin from '../../common/mixins/popup'
   import Cartcontrol from '../cartcontrol/cartcontrol'
+  import RatingSelect from '../rating-select/rating-select'
+  import moment from 'moment'
 
   const EVENT_SHOW = 'show'
   const EVENT_LEAVE = 'leave'
   const EVENT_ADD = 'add'
+  const ALL = 2
   export default {
     name: 'food',
     mixins: [popupMixin],
     props: {
       food: {
         type: Object
+      }
+    },
+    data () {
+      return {
+        onlyContent: true,
+        selectType: ALL,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
+      }
+    },
+    computed: {
+      ratings () {
+        return this.food.ratings
+      },
+      computedRatings () {
+        const ret = []
+        this.ratings.forEach((rating) => {
+          if (this.onlyContent && !rating.text) {
+            return
+          }
+          if (this.selectType === ALL || this.selectType === rating.rateType) {
+            ret.push(rating)
+          }
+        })
+        return ret
       }
     },
     created () {
@@ -75,11 +139,21 @@
       },
       addFood (target) {
         this.$emit(EVENT_ADD, target)
+      },
+      format (time) {
+        return moment(time).format('YYYY-MM-DD hh:mm')
+      },
+      onSelect (type) {
+        this.selectType = type
+      },
+      onToggle () {
+        this.onlyContent = !this.onlyContent
       }
     },
     components: {
       Cartcontrol,
-      Split
+      Split,
+      RatingSelect
     }
   }
 </script>
@@ -96,25 +170,31 @@
     z-index: 30
     width: 100%
     background: $color-white
+
     &.move-enter-active, &.move-leave-active
       transition: all 0.3s linear
+
     &.move-enter, &.move-leave-active
       transform: translate3d(100%, 0, 0)
+
     .image-header
       position: relative
       width: 100%
       height: 0
       padding-top: 100%
+
       img
         position: absolute
         top: 0
         left: 0
         width: 100%
         height: 100%
+
       .back
         position: absolute
         top: 10px
         left: 0
+
         .icon-arrow_lift
           display: block
           padding: 10px
@@ -124,36 +204,45 @@
     .content
       position: relative
       padding: 18px
+
       .title
         line-height: 14px
         margin-bottom: 8px
         font-size: $fontsize-medium
         font-weight: 700
         color: $color-dark-grey
+
       .detail-content
         margin-bottom: 18px
         line-height: 10px
         height: 10px
+
         .sell-count, .rating
           font-size: $fontsize-small-s
           color: $color-light-grey
+
         .sell-count
           margin-right: 12px
+
       .price
         line-height: 24px
         font-weight: 700
+
         .now
           margin-right: 8px
           font-size: 14px
           color: $color-red
+
         .old
           text-decoration: line-through
           font-size: $fontsize-small-s
           color: $color-light-grey
+
       .cart-control-wrapper
         position: absolute
         right: 12px
         bottom: 12px
+
       .buy
         position: absolute
         right: 18px
@@ -168,37 +257,48 @@
         color: $color-white
         background: $color-blue
         opacity: 1
+
         &.fade-enter-active, &.fade-leave-active
           transition: all 0.3s
+
         &.fade-enter, &.fade-leave-active
           opacity: 0
           z-index: -1
+
     .info
       padding: 18px
+
       .title
         line-height: 14px
         margin-bottom: 6px
         font-size: $fontsize-medium
         color: $color-dark-grey
+
       .text
         line-height: 24px
         padding: 0 8px
         font-size: $fontsize-small
         color: $color-grey
+
     .rating
       padding-top: 18px
+
       .title
         line-height: 14px
         margin-left: 18px
         font-size: $fontsize-medium
         color: $color-dark-grey
+
       .rating-wrapper
         padding: 0 18px
+
         .rating-item
           position: relative
           padding: 16px 0
+
           &:last-child
             border-none()
+
           .user
             position: absolute
             right: 0
@@ -206,27 +306,34 @@
             display: flex
             align-items: center
             line-height: 12px
+
             .name
               margin-right: 6px
               font-size: $fontsize-small-s
               color: $color-light-grey
+
             .avatar
               border-radius: 50%
+
           .time
             margin-bottom: 6px
             line-height: 12px
             font-size: $fontsize-small-s
             color: $color-light-grey
+
           .text
             line-height: 16px
             font-size: $fontsize-small
             color: $color-dark-grey
+
             .icon-thumb_up, .icon-thumb_down
               margin-right: 4px
               line-height: 16px
               font-size: $fontsize-small
+
             .icon-thumb_up
               color: $color-blue
+
             .icon-thumb_down
               color: $color-light-grey
 
